df43d8137c1c8512c2b1bd0af4472852d7c6fb1b
Moritz Ney <mney@avarteq.de>
2017-08-14 15:23:39 +0200

    add book js and update main layout

--- src/App/index.js
+++ src/App/index.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
+import Book from '../Book/Book';
 import { Layout, Menu, Icon } from 'antd';
 import '../App.css';
 
 const { Sider, Content } = Layout;
-const mockBooksList = [
-  {id: 1, title: 'Book1', description: 'desc1', status: 'READ'},
-  {id: 2, title: 'Book2', description: 'desc2', status: 'READ'},
-  {id: 3, title: 'Book3', description: 'desc3', status: 'READ'},
-];
 
 class App extends Component {
   render() {
@@ -15,20 +11,21 @@ class App extends Component {
       <div className="App">
 
         <Layout>
-          <Sider width={250} style={{ height: '100vh' }}>
+          <Sider className='book-list' width={250} style={{ height: '100vh' }}>
+            <h3>Book list</h3>
+
             <Menu theme="dark" mode="inline">
               { mockBooksList.map((bookItem) => <Menu.Item key={bookItem.id}>
                   <Icon type='book' />
                   <span className="nav-text">{bookItem.title}</span>
                 </Menu.Item>
               )}
+
               <Menu.Divider />
             </Menu>
           </Sider>
 
-          <Content style={{ margin: '20px' }}>
-            Main Content
-          </Content>
+          <Book />
         </Layout>
       </div>
     );
---
+++ src/Book/Book.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import { Layout } from 'antd';
+const { Content } = Layout;
+
+const RenderBook = ({bookId, books}) => {
+
+  const emptyBook = {title: "Please select a book :-)", description: ''};
+  const bookIndex = _.findIndex(books, x => x.id === bookId);
+  const book = bookIndex !== -1 ? books[bookIndex] : emptyBook;
+
+  return (
+    <Content className='book-content'>
+      <h1>
+
+      </h1>
+
+      <p>
+
+      </p>
+
+    </Content>
+  );
+}
+
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    bookId: state.BooksReducer.bookId,
+    books: state.BooksReducer.books,
+  }
+}
+
+const Book = connect(
+  mapStateToProps,
+)(RenderBook)
+
+export default Book;
