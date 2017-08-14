c6eda9d70076345d873d8abf4f13d135b82726ad
Moritz Ney <mney@avarteq.de>
2017-08-14 15:46:32 +0200

    add store variable and provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import bookApp from './Book/Reducers';
 import './index.css';
 import App from './App';
-import bookApp from './Book/Reducers';
 import registerServiceWorker from './registerServiceWorker';
 
+let store = createStore(bookApp);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 registerServiceWorker();
